9c6cd6fd568f4538d4ac33a8458be23d4709d1c5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-07-21 11:59:41 -0300

    feat(stencil): add stencil project

---
+++ stencil/stencil.config.ts
@@ -0,0 +1,19 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'natds-js-labs-stencil',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
